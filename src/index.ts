8b9263087949b54e3d8c52f82013d4f1379e309a
Thomas Zerr <thomas.zerr@gmail.com>
2021-10-28 22:12:59 +0200

    -added: user control

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,47 @@
 import { Maze } from './maze/Maze'
+import terminal from 'terminal-kit'
 
-const maze = new Maze(16, 16)
+const term = terminal.terminal
+const maze = new Maze(32, 16)
 maze.generateMazeWithRandomPrim()
 
-console.log(maze)
+term(maze.toString())
+
+term.grabInput(true)
+
+term.on('key', function (name: string) {
+  switch (name) {
+    case 'CTRL_C': {
+      process.exit(0)
+    }
+    case 'UP': {
+      if (maze.movePlayerUp()) {
+        rerenderMaze(maze, term)
+      }
+      break
+    }
+    case 'DOWN': {
+      if (maze.movePlayerDown()) {
+        rerenderMaze(maze, term)
+      }
+      break
+    }
+    case 'LEFT': {
+      if (maze.movePlayerLeft()) {
+        rerenderMaze(maze, term)
+      }
+      break
+    }
+    case 'RIGHT': {
+      if (maze.movePlayerRight()) {
+        rerenderMaze(maze, term)
+      }
+      break
+    }
+  }
+})
+
+function rerenderMaze (maze: Maze, term: terminal.Terminal) {
+  term.clear()
+  term(maze.toString())
+}
