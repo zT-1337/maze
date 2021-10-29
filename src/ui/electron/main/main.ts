66e6a7f23e51d4e16ff522c7d99435a929294b7a
Thomas Zerr <thomas.zerr@gmail.com>
2021-10-29 13:04:21 +0200

    -added: rendering of maze

--- src/ui/electron/main/main.ts
+++ src/ui/electron/main/main.ts
@@ -17,8 +17,8 @@ export function initApp () {
 
 function createWindow () {
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true
--- src/ui/electron/main/preload.ts
+++ src/ui/electron/main/preload.ts
@@ -1,10 +1,1 @@
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector: string, text: string | undefined) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text || 'unknown Version'
-  }
 
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
-})
