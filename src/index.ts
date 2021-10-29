564438dcdfc8f796d24af22df5ec66b372d63e6d
Thomas Zerr <thomas.zerr@gmail.com>
2021-10-29 09:02:15 +0200

    -moved: terminal ui logic to own file

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,7 @@
 import { Maze } from './maze/maze'
-import terminal from 'terminal-kit'
-
-const term = terminal.terminal
-term.clear()
+import { start } from './ui/terminal'
 
 const maze = new Maze(32, 16)
 maze.generateMazeWithRandomPrim()
 
-term(maze.toString())
-
-term.grabInput(true)
-
-term.on('key', function (name: string) {
-  switch (name) {
-    case 'CTRL_C': {
-      process.exit(0)
-    }
-    case 'UP': {
-      if (maze.movePlayerUp()) {
-        rerenderMaze(maze, term)
-      }
-      break
-    }
-    case 'DOWN': {
-      if (maze.movePlayerDown()) {
-        rerenderMaze(maze, term)
-      }
-      break
-    }
-    case 'LEFT': {
-      if (maze.movePlayerLeft()) {
-        rerenderMaze(maze, term)
-      }
-      break
-    }
-    case 'RIGHT': {
-      if (maze.movePlayerRight()) {
-        rerenderMaze(maze, term)
-      }
-      break
-    }
-  }
-})
-
-function rerenderMaze (maze: Maze, term: terminal.Terminal) {
-  term.clear()
-  term(maze.toString())
-}
+start(maze)
---
+++ src/ui/terminal.ts
@@ -0,0 +1,49 @@
+import { Maze } from '../maze/maze'
+import terminal from 'terminal-kit'
+
+export function start (maze: Maze) {
+  const term = terminal.terminal
+  renderMaze(maze, term)
+  setupInput(maze, term)
+}
+
+function setupInput (maze: Maze, term: terminal.Terminal) {
+  term.grabInput(true)
+
+  term.on('key', function (name: string) {
+    switch (name) {
+      case 'CTRL_C': {
+        process.exit(0)
+      }
+      case 'UP': {
+        if (maze.movePlayerUp()) {
+          renderMaze(maze, term)
+        }
+        break
+      }
+      case 'DOWN': {
+        if (maze.movePlayerDown()) {
+          renderMaze(maze, term)
+        }
+        break
+      }
+      case 'LEFT': {
+        if (maze.movePlayerLeft()) {
+          renderMaze(maze, term)
+        }
+        break
+      }
+      case 'RIGHT': {
+        if (maze.movePlayerRight()) {
+          renderMaze(maze, term)
+        }
+        break
+      }
+    }
+  })
+}
+
+function renderMaze (maze: Maze, term: terminal.Terminal) {
+  term.clear()
+  term(maze.toString())
+}
